6c8fff7d3bb5b77ece591236b36eb9bd6c67705e
ekatsevi <katsevich.gene@gmail.com>
2023-08-27 19:18:33 -0400

    Minor tweaks for 2023

--- problem-2.R
+++ problem-2.R
@@ -4,21 +4,19 @@ library(tidyverse)
 # part (a)
 ############################################################
 
-# generate the data 
+# generate the data
 n <- 1000
 X <- rnorm(n)
 
 # plot the data
-p_hist <- tibble(X) %>%
+p_hist <- tibble(X) |>
   ggplot(aes(x = X)) +
   geom_histogram(color = "black") +
   theme_bw()
 
 # save plot to disk
-ggsave(filename = "figures-and-tables/normal-histogram.pdf", 
-       plot = p_hist, 
-       device = "pdf", 
+ggsave(filename = "figures-and-tables/normal-histogram.pdf",
+       plot = p_hist,
+       device = "pdf",
        width = 4,
        height = 3)
-
-#### make lots of changes!!!!
--- problem-3.R
+++ problem-3.R
@@ -6,16 +6,16 @@ library(kableExtra)
 ############################################################
 
 # create the histogram of diamond price
-p_hist <- diamonds %>%
+p_hist <- diamonds |>
   ggplot(aes(x = price)) +
   geom_histogram(color = "black") +
   labs(x = "Price (in dollars)") +
   theme_bw()
 
 # save plot to disk
-ggsave(filename = "figures-and-tables/price-histogram.pdf", 
-       plot = p_hist, 
-       device = "pdf", 
+ggsave(filename = "figures-and-tables/price-histogram.pdf",
+       plot = p_hist,
+       device = "pdf",
        width = 4,
        height = 3)
 
@@ -25,22 +25,22 @@ ggsave(filename = "figures-and-tables/price-histogram.pdf",
 ############################################################
 
 # compute the mean diamond price by cut
-price_by_cut <- diamonds %>% 
-  group_by(cut) %>%
-  summarise(mean_price = mean(price)) %>%
+price_by_cut <- diamonds |>
+  group_by(cut) |>
+  summarise(mean_price = mean(price)) |>
   rename(Cut = cut, `Mean Price ($)` = mean_price)
 
 # format and save table
-price_by_cut %>%
-  kable(format = "latex", 
+price_by_cut |>
+  kable(format = "latex",
         row.names = NA,
         booktabs = TRUE,
         digits = 2,
         caption = "Mean diamond price by cut.",
-        label = "diamond-price-by-cut") %>%
-  kable_styling(latex_options = "hold_position") %>%
+        label = "diamond-price-by-cut") |>
+  kable_styling(latex_options = "hold_position") |>
   save_kable("figures-and-tables/diamond-price-by-cut.tex")
-  
+
 ############################################################
 # part (c)
 ############################################################
